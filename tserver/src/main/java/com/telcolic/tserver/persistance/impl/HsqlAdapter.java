5197e6b5853a3cf57fd3090865e63b9fccc6dab5
ekemen <huseyin.ekemen@kron.com.tr>
2016-06-15 15:56:13 +0300

    Adapter wrappers

--- tserver/src/main/java/com/telcolic/tserver/persistance/impl/HsqlAdapter.java
+++ tserver/src/main/java/com/telcolic/tserver/persistance/impl/HsqlAdapter.java
@@ -1,7 +1,19 @@
 package com.telcolic.tserver.persistance.impl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by h2e on 15/06/16.
  */
 public class HsqlAdapter {
+
+    public HsqlAdapter() {
+        try {
+            Connection c = DriverManager.getConnection("jdbc:hsqldb:mem:mymemdb", "SA", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
